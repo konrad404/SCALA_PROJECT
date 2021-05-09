8ff096f070e71257abd5342bf67c6dd1eb3487d4
Adam <adamniemiec862@gmail.com>
2021-05-09 19:43:22 +0200

    y ok

---
+++ Customer.scala
@@ -0,0 +1,11 @@
+import Enums.Direction._
+import Enums.TripReason._
+
+package Customer{
+class Customer(private val name: String, private val countries: Array[Direction], private var numberOfCustomers: Int,
+    private var daysInAdvance: Int, private val priceRange: Array[Int], private val tripReason: TripReason){
+
+    
+        
+} 
+}
