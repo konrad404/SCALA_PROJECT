334e245bac1596f956e9484cd88a42a9d37e9420
Konrad <kczerepakto@gmial.com>
2021-05-11 16:56:00 +0200

    woow

--- Flight.scala
+++ Flight.scala
@@ -2,6 +2,7 @@ package Flight{
     import Enums.Direction._
 
     import java.util.Calendar;
+    import java.util.Date
 
     object Status extends Enumeration{
         type InOut = Value
@@ -12,55 +13,53 @@ package Flight{
     import Status._
 
 
-    class Data(private val year: Int, private val month: Int, private val day: Int){
-        override def toString(): String = "Y: " + year + " M: " + month + " D: " + day
-
-        def howFarAway(): Int ={
-            var dT = Calendar.getInstance()
-            println(dT.getTime())
-            1
+    class DateFun(){
+        // var c = Calendar()
+        def getDate(): Date = {
+            Calendar.getInstance().getTime()
         }
+
     }
 
-    class Hour(private var hour: Int, private var minute: Int){
-        override def toString(): String = {
-            var h = {
-                if(hour < 10)
-                "0" + hour
-                else
-                "" + hour
-            }
-            var m = {
-                if(minute < 10)
-                "0" + minute
-                else
-                "" + minute
-            }
-            h + ":" + m
-        }
-        def increase(bonusHour: Int, bonusMinute: Int): Int = {
-            var bHour = bonusHour
-            minute += bonusMinute
-            if(minute >= 60){
-                minute %= 60
-                bHour +=1
-            }
-            hour += bHour
-            if(hour >= 24){
-                hour %= 24
-                1
-            }
-            else{
-                0
-            }
-        }
-    }
+    // class Hour(private var hour: Int, private var minute: Int){
+    //     override def toString(): String = {
+    //         var h = {
+    //             if(hour < 10)
+    //             "0" + hour
+    //             else
+    //             "" + hour
+    //         }
+    //         var m = {
+    //             if(minute < 10)
+    //             "0" + minute
+    //             else
+    //             "" + minute
+    //         }
+    //         h + ":" + m
+    //     }
+    //     def increase(bonusHour: Int, bonusMinute: Int): Int = {
+    //         var bHour = bonusHour
+    //         minute += bonusMinute
+    //         if(minute >= 60){
+    //             minute %= 60
+    //             bHour +=1
+    //         }
+    //         hour += bHour
+    //         if(hour >= 24){
+    //             hour %= 24
+    //             1
+    //         }
+    //         else{
+    //             0
+    //         }
+    //     }
+    // }
 
     class InvalidProbabilityException(s:String) extends Exception(s){}  
 
 
-    class Flight(private val direction: Direction, private val in_out: InOut, private var places: Int, private val data: Data,
-    private val hour: Hour, private val changeTimeProb: Double, private val runway: Int, private val price: Double){
+    class Flight(private val direction: Direction, private val in_out: InOut, private var places: Int, private val date: Date,
+     private val changeTimeProb: Double, private val runway: Int, private val price: Double){
         if(places <= 0){
             throw new IllegalArgumentException
         }
@@ -71,8 +70,7 @@ package Flight{
         def getPrice(): Double = price
         def isArriving(): Boolean  = (in_out == Status.Arriving)
         def isLeaving(): Boolean  = (in_out == Status.Leaving)
-        def getDate(): Data = data
-        def getHour(): Hour = hour
+        def getDate(): Date = date
         def res_places(n: Int): Unit ={
             if(n > places){
                 throw new IllegalArgumentException("There is not enough places")
@@ -84,26 +82,21 @@ package Flight{
     }
 
 
-    object Appl {
-    def main(agrs: Array[String]) {
-        val data = new Data(1997, 12, 14)
-        data.howFarAway()
-        val direction = London
-        val inOut = Status.Arriving
-        val hour = new Hour(23,57)
-        val newFlight = new Flight(direction,inOut,20,data,hour,0.3,5,54.43)
-        println(newFlight.getPrice())
-        println(newFlight.isArriving())  
-        println(newFlight.isLeaving())
-        println(newFlight.getDate())
-        println(newFlight.getPlacesNumber())
-        println(newFlight.getDirection())
-        println(newFlight.getHour())
-        println(hour)
-        val day = hour.increase(2,4)
-        println(day +" godzina: " + hour)
-    }
-    }
+    // object ApplFlight {
+    //     def main(agrs: Array[String]) {
+    //         val data = new Date(1997, 12, 14)
+    //         // println(data.howFarAway())
+    //         val direction = London
+    //         val inOut = Status.Arriving
+    //         val newFlight = new Flight(direction,inOut,20,data,0.3,5,54.43)
+    //         println(newFlight.getPrice())
+    //         println(newFlight.isArriving())  
+    //         println(newFlight.isLeaving())
+    //         println(newFlight.getDate())
+    //         println(newFlight.getPlacesNumber())
+    //         println(newFlight.getDirection())
+    //     }
+    // }
 }
 
 
