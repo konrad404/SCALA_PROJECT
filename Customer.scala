978f6db13d051d9042bd81c859a378adf3d16635
Adam <adamniemiec862@gmail.com>
2021-05-14 10:04:08 +0200

    nie działa mi to

--- Customer.scala
+++ Customer.scala
@@ -1,11 +1,27 @@
 import Enums.Direction._
 import Enums.TripReason._
+import Engine.Engine._
+import Flight.Flight._
 
 package Customer{
 class Customer(private val name: String, private val countries: Array[Direction], private var numberOfCustomers: Int,
-    private var daysInAdvance: Int, private val priceRange: Array[Int], private val tripReason: TripReason){
+    private var daysInAdvance: Int, private val priceRange: Array[Int], private val tripReason: TripReason, private val engine: Engine){
 
-    
+    private val bookedFlights : Array[Flight] = Array()
+
+    def bookFlight():Unit={
+        val flights : Array[Flight] = this.engine.getFlights
+
+        //flights.filter(f => ())
+    }
         
 } 
 }
+
+/*
+zamów lot
+odwołaj lot
+przełóż na następny termin
+przyjedź na lot
+dokupić bilet
+*/
--- Engine.scala
+++ Engine.scala
@@ -5,5 +5,7 @@ import Enums._
 package Engine{
 
 class Engine(){}
+    private var flights : Array[Flight] = Array() // to się wygeneruje
 
+    def getFlights():Array[Flight]={this.flights}
 }
