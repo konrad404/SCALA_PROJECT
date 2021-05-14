0a6e080faf5127af661b0c5db093b31f8a5ffa67
Konrad <kczerepakto@gmial.com>
2021-05-14 11:49:44 +0200

    nowy

--- Enums.scala
+++ Enums.scala
@@ -8,4 +8,14 @@ object TripReason extends Enumeration {
     type TripReason = Value
     val Vacation, Work, Visit = Value
 }
+
+object Sizes extends Enumeration {
+    val Small = Value("80")
+    val Medium = Value("160")
+    val Big = Value("220")
+}
+object Runaways extends Enumeration {
+    type Runaways = Value
+    val One, Two, Three, Four, Five, Six, Seven, Eight = Value
+}
 }
--- Flight.scala
+++ Flight.scala
@@ -1,17 +1,19 @@
 package Flight{
-    import Enums.Direction._
-
-    import java.util.Calendar;
+    import Enums.Direction
+    import Enums.Sizes
+    import Enums.Runaways
+    import java.util.Calendar
     import java.util.Date
+    import scala.util.Random
+    import scala.math.BigDecimal
 
     object Status extends Enumeration{
-        type InOut = Value
+        type Status = Value
         val Leaving, Arriving, Staying, Left = Value
     }
 
 
-    import Status._
-
+    // import Status
 
     class DateFun(){
         // var c = Calendar()
@@ -58,27 +60,68 @@ package Flight{
     class InvalidProbabilityException(s:String) extends Exception(s){}  
 
 
-    class Flight(private val direction: Direction, private val in_out: InOut, private var places: Int, private val date: Date,
-     private val changeTimeProb: Double, private val runway: Int, private val price: Double){
-        if(places <= 0){
+    class Flight(private val direction: Direction.Value, private val status: Status.Value, private var freePlaces: Int, private val date: Date,
+     private val changeTimeProb: Double, private val runway: Runaways.Value, private val price: Double){
+        if(freePlaces <= 0){
             throw new IllegalArgumentException
         }
         if(changeTimeProb > 1 || changeTimeProb < 0){
             throw new InvalidProbabilityException("wrong probability")
         }
+        val toAdd = 200
+
+        private val firstClassPrice = price + toAdd
+        private val places = freePlaces
 
         def getPrice(): Double = price
-        def isArriving(): Boolean  = (in_out == Status.Arriving)
-        def isLeaving(): Boolean  = (in_out == Status.Leaving)
+        def getFirstClassPrice(): Double = firstClassPrice
+        def isArriving(): Boolean  = (status == Status.Arriving)
+        def isLeaving(): Boolean  = (status == Status.Leaving)
         def getDate(): Date = date
         def res_places(n: Int): Unit ={
-            if(n > places){
+            if(n > freePlaces){
                 throw new IllegalArgumentException("There is not enough places")
             }
-            places -= n
+            freePlaces -= n
         }
-        def getPlacesNumber(): Int = places
-        def getDirection(): Direction = direction;
+        def getFreePlacesNumber(): Int = freePlaces
+        def getDirection(): Direction.Direction = direction    
+        def getProbability(): Double = changeTimeProb
+        override def toString() = {
+            "Direction: " + direction + ", date: " + date + ", status: " + status + ", places: " + places + ", free: " + freePlaces + ", price:" + price + ", first class price: " + firstClassPrice
+        }
+    }
+
+    class FlightGenerator(){
+        val minPrice = 50
+        val priceBracket = 300
+        def generateFlight(): Flight = {
+            val today = new Date()
+            var time = today.getTime()
+            var seed: Long = scala.math.pow(10,10).toLong
+            var toAdd = Random.nextLong(seed)
+            println(toAdd) 
+            val data = new Date(time + toAdd)
+            for( i <- Sizes.values){
+                println(i)
+            }
+            val places = Sizes(Random.nextInt(Sizes.maxId)).toString().toInt
+            val probability = Random.nextDouble()
+            val direction = Direction(Random.nextInt(Direction.maxId))
+            val inOut = Status(Random.nextInt(2))
+            val runaway = Runaways(Random.nextInt(Runaways.maxId))
+            val price = (minPrice + Random.nextInt(priceBracket)).toDouble
+            new Flight(direction,inOut,places,data,probability,runaway,price)
+        }
+        
+        def generateTimetable(amount:  Int){
+            
+        }
+
+    }
+
+    object FlightGenerator {
+        def apply() = new FlightGenerator
     }
 
 
